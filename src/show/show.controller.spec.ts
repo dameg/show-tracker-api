476b9cac2458a0bf26e5d630f2819684b1879962
Damian Dynur <damiandynur@icloud.com>
2019-09-14 19:02:16 +0200

    tests

--- src/show/show.controller.spec.ts
+++ src/show/show.controller.spec.ts
@@ -1,6 +1,9 @@
 import { Test } from '@nestjs/testing';
 import { ShowController } from './show.controller';
 import { ShowService } from './show.service';
+import { TypeOrmModule, getRepositoryToken } from '@nestjs/typeorm';
+import { Show } from './show.entity';
+import { Repository } from 'typeorm';
 
 describe('ShowController', () => {
   let showController: ShowController;
@@ -9,20 +12,28 @@ describe('ShowController', () => {
   beforeEach(async () => {
     const module = await Test.createTestingModule({
       controllers: [ShowController],
-      providers: [ShowService],
+      providers: [ShowService,
+         {
+        provide: getRepositoryToken(Show),
+        useClass: Show
+      }
+   ],
+
     }).compile();
 
     showService = module.get<ShowService>(ShowService);
     showController = module.get<ShowController>(ShowController);
+
   });
 
   describe('showIndex', () => {
     it('should return an array of shows', async () => {
       const result = [{ id: 1, name: 'Orange is the new black', episode: [] }];
-      jest
-        .spyOn(showService, 'showIndex')
+      jest.spyOn(showService, 'showIndex')
         .mockImplementation(() => Promise.resolve(result));
+
       expect(await showController.showIndex()).toBe(result);
+
     });
   });
 });
